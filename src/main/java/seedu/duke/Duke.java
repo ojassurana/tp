d1c114e61f975421b04d10a2aed0550e74fb1ebc
LeThiHongMinh <hongminhle2005@gmail.com>
2025-03-16 15:41:24 +0800

    Fix Duke test issue

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,4 +1,4 @@
-package seedu;
+package seedu.duke;
 
 import java.util.Scanner;
 
--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,4 +1,4 @@
-package seedu;
+package seedu.duke;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
