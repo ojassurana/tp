e7d5d6a370d9b698153750e7bb4a310d795715e5
LeThiHongMinh <hongminhle2005@gmail.com>
2025-03-16 15:34:10 +0800

    Fix Duke test missing issue

--- src/test/java/TripManagerTest.java
+++ src/test/java/TripManagerTest.java
@@ -2,8 +2,6 @@ import trip.TripManager;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class TripManagerTest {
     private TripManager tripManager;
--- src/test/java/TripTest.java
+++ src/test/java/TripTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import trip.Trip;
 
 public class TripTest {
