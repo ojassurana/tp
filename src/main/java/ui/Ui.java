3319e83d1353823ce9248ec0324c5cb99a1a0a0e
ojassurana <me@ojassurana.com>
2025-03-17 16:27:25 +0800

    Added scanner input into Ui class.

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -1,4 +1,19 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Ui {
+    private final Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readInput() {
+        return scanner.nextLine();
+    }
+
+    public void close() {
+        scanner.close();
+    }
 }
