c8847bf7efc17f65f7e11c8a06044d2061cbff04
ashleyang2001 <ashleyang2001@live.com>
2025-04-07 14:44:44 +0800

    Added TrackerTest, modified PhotoPrinterTest.
    Abstracted same haversine method from PhotoMetadataExtractor to Tracker class.
    
    Slight modifications on UI to show user what page they are at.
    
    Added comments for Tracker and PhotoDateTimeComparator Classes.

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -1,11 +1,14 @@
 package ui;
 
 import java.util.Scanner;
-
 import parser.Parser;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Ui {
-    private static final String SEPARATOR_LINE = "____________________________________________________________\n";
+    private static final String SEPARATOR_LINE = "____________________________________________________________";
+    private static final int PADDING = 25;
+    private static final String FILE_PATH = "./data/assets/van.txt";
     public static final String[] COMMAND_ARRAY = Parser.COMMAND_ARRAY;
     private final Scanner scanner;
 
@@ -14,6 +17,7 @@ public class Ui {
     }
 
     public String readInput() {
+
         return scanner.nextLine();
     }
 
@@ -21,6 +25,7 @@ public class Ui {
         System.out.println("Welcome to your Travel Diary Management System!");
         System.out.println("Capture and manage your travel memories with ease!\n");
         System.out.println("Please type \"help\" if you need instructions \n");
+        this.showLogo();
     }
 
     public void showLine() {
@@ -40,15 +45,30 @@ public class Ui {
     }
 
     public void showTripPage() {
-        System.out.println("*********");
-        System.out.println("Trip Page");
-        System.out.println("*********");
+        System.out.println(SEPARATOR_LINE);
+        System.out.println(" ".repeat(PADDING) + "|Trip Page|");
     }
 
     public void showPhotoPage() {
-        System.out.println("**********");
-        System.out.println("Photo Page");
-        System.out.println("**********");
+        System.out.println(SEPARATOR_LINE);
+        System.out.println(" ".repeat(PADDING) + "|Photo Page|");
+    }
+
+    public void showLogo() {
+        try {
+            File file = new File(FILE_PATH);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            // Handle the exception if the file is not found
+            System.err.println("File not found: " + e.getMessage());
+        }
     }
 
     public void close() {
---
+++ src/test/java/TrackerTest.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Test;
+import photo.Photo;
+import album.Album;
+import tracker.Tracker;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TrackerTest {
+
+    /**
+     * Tests sorting photos by date.
+     * Verifies that the photos are correctly sorted based on their datetime.
+     */
+    @Test
+    void sortPhotosByDate_withValidPhotoList_shouldSortCorrectly() {
+        try {
+            Photo photo1 = new Photo("./data/photos/hongkong_1.jpeg", "photo 1 name", "photo 1 caption");
+            Photo photo2 = new Photo("./data/photos/hongkong_2.jpeg", "photo 2 name", "photo 2 caption");
+            Photo photo3 = new Photo("./data/photos/hongkong_3.jpeg", "photo 3 name", "photo 3 caption");
+            List<Photo> photoList = new ArrayList<>();
+            photoList.add(photo1);
+            photoList.add(photo2);
+            photoList.add(photo3);
+
+            Tracker.sortPhotosByDate(photoList);
+
+            // Verify the order of sorted photos
+            assertEquals(photo2, photoList.get(0)); // Earliest photo
+            assertEquals(photo1, photoList.get(1)); // Second earliest photo
+            assertEquals(photo3, photoList.get(2)); // Latest photo
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    /**
+     * Tests calculating the distance between two photos based on their locations.
+     * Verifies the accuracy of the calculated distance using expected values.
+     */
+    @Test
+    void getDist_withTwoValidPhotos_shouldCalculateCorrectDistance() {
+        try {
+            Photo photo1 = new Photo("./data/photos/hongkong_1.jpeg", "photo 1 name", "photo 1 caption");
+            Photo photo2 = new Photo("./data/photos/hongkong_2.jpeg", "photo 2 name", "photo 2 caption");
+            // Calculate the distance
+            double distance = Tracker.getDist(photo1, photo2);
+            System.out.println(distance);
+            assertEquals(14.0, distance);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Tests retrieving the period of an album by finding the earliest and latest dates of its photos.
+     * Verifies that the period is correctly calculated.
+     */
+    @Test
+    void getPeriod_withValidAlbum_shouldReturnCorrectDates() {
+        try {
+            Album album = new Album();
+            album.addPhoto("./data/photos/hongkong_1.jpeg", "photo 1 name", "photo 1 caption");
+            album.addPhoto("./data/photos/hongkong_2.jpeg", "photo 2 name", "photo 2 caption");
+            album.addPhoto("./data/photos/hongkong_2.jpeg", "photo 3 name", "photo 3 caption");
+
+            // Get the period
+            List<String> minMaxDates = Tracker.getPeriod(album);
+
+            // Verify the start and end dates
+            assertEquals("2024-11-16 12:32PM", minMaxDates.get(0)); // Earliest date
+            assertEquals("2024-11-17 1:16PM", minMaxDates.get(1)); // Latest date
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
